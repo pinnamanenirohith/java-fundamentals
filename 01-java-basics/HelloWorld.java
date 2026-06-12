406bc2ad277f119d077f642d689d84c2c17a29ee
Pinnamaneni Rohith Venkata Sai <pinnamanenirohith@gmail.com>
2026-06-12 20:01:42 +0530

    Create HelloWorld.java with basic print statements
    Added a simple Java program that prints messages to the console.

---
+++ 01-java-basics/HelloWorld.java
@@ -0,0 +1,34 @@
+/**
+ * HelloWorld.java
+   * Topic: Java Basics
+      *
+      * Description:
+    * The simplest Java program that prints "Hello, World!" to the console.
+      * This is the traditional first program every Java developer writes.
+      *
+      * Concepts Covered:
+    * - Class declaration
+       * - main() method
+      * - System.out.println()
+      *
+      * Author: Pinnamaneni Rohith Venkata Sai
+      * Course: Wipro TalentNext (WTN) - Java Fundamentals
+      */
+public class HelloWorld {
+
+    /**
+     * The main method is the entry point of every Java application.
+       * The JVM calls this method to start the program.
+       *
+       * @param args command-line arguments (not used here)
+       */
+    public static void main(String[] args) {
+
+          // Print a message to the console
+          System.out.println("Hello, World!");
+
+          // Additional beginner-friendly output
+          System.out.println("Welcome to Java Programming!");
+              System.out.println("This is my first Java program.");
+    }
+}
